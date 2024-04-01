cac7ae9d8c1eb0162f4b488a3245ce44020bd97f
Pablo Burgos <pabloburgosc@gmail.com>
2024-03-31 22:27:33 -0300

    primer commit

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(document).click(function (event) {
+    var clickover = $(event.target);
+    var $navbar = $(".navbar-collapse");
+    var _opened = $navbar.hasClass("show");
+    if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+      $navbar.collapse("hide");
+    }
+  });
+});
