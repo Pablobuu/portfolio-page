409af7e58f7c4f0369ca9a50327ebb4710ba10e1
Pablo Burgos <pabloburgosc@gmail.com>
2024-04-02 01:27:16 -0300

    avance en estilos y contenidos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
+
 $(document).ready(function () {
   $(document).click(function (event) {
     var clickover = $(event.target);
